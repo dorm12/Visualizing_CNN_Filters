0f2ee827a9d570018d78647d5d6ed722ecf2594a
dorm12 <dor1213@gmail.com>
2022-11-17 14:24:31 +0200

    ...

--- API/Visualizing_Filters.py
+++ API/Visualizing_Filters.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import cv2
 
 def compute_loss(input_image, filter_index, feature_extractor):
     activation = feature_extractor(input_image)
@@ -17,13 +18,13 @@ def gradient_ascent_step(img, filter_index, learning_rate, feature_extractor):
     grads = tape.gradient(loss, img)
     # Normalize gradients.
     grads = tf.math.l2_normalize(grads)
-    img += learning_rate * grads
+    img = img + learning_rate * grads
     return loss, img
 
 
-def initialize_image(img_width, img_height, custom):
+def initialize_image(img_width, img_height, color_channels, custom):
     # We start from a gray image with some random noise
-    img = tf.random.uniform((1, img_width, img_height, 3))
+    img = tf.random.uniform((1, img_width, img_height, color_channels))
     if custom:
         return img
     # ResNet50V2 expects inputs in the range [-1, +1].
@@ -31,32 +32,46 @@ def initialize_image(img_width, img_height, custom):
     return (img - 0.5) * 0.25
 
 
-def visualize_filter(filter_index, feature_extractor, img_width=180, img_height=180, custom=True, initializer=None):
-    # We run gradient ascent for 20 steps
-    iterations = 30
-    learning_rate = 10.0
+def visualize_filter(filter_index, feature_extractor, 
+                     img_width=120, img_height=120,
+                     color_channels=3,
+                     custom=True, initializer=None, 
+                     iterations=30, learning_rate = 10.0,
+                     return_deprocessed=True):
     if initializer:
-        img = initializer(img_width, img_height)
+        img = tf.Variable(initializer(img_width, img_height, color_channels))
     else:
-        img = initialize_image(img_width, img_height, custom)
+        img = initialize_image(img_width, img_height, color_channels, custom)
+        
+    if img.shape[:-1]!=(img_height, img_width):
+        img = cv2.resize(img, (img_height, img_width), cv2.)
+        
+    if img.max()>1:
+        img = img/img.max()
+        
     for iteration in range(iterations):
         loss, img = gradient_ascent_step(img, filter_index, learning_rate, feature_extractor)
 
-    # Decode the resulting input image
-    img = deprocess_image(img[0].numpy(), custom)
-    return loss, img
+    if return_deprocessed:
+        # Decode the resulting input image
+        img = deprocess_image(img[0].numpy(), custom)
+        return loss, img
+    
+    else:
+        return loss, np.clip(img[0].numpy(), 0, 1)
 
 
 def deprocess_image(img, custom):
+    border = int(len(img)*.1)
     if custom:
-        return np.clip(img[25:-25, 25:-25, :], 0, 1)
+        return np.clip(img[border:-border, border:-border, :], 0, 1)
     # Normalize array: center on 0., ensure variance is 0.15
     img -= img.mean()
     img /= img.std() + 1e-5
     img *= 0.15
 
     # Center crop
-    img = img[25:-25, 25:-25, :]
+    img = img[border:-border, border:-border, :]
 
     # Clip to [0, 1]
     img += 0.5
@@ -66,8 +81,3 @@ def deprocess_image(img, custom):
     # img *= 255
     # img = np.clip(img, 0, 255).astype("uint8")
     return img
-
-
-    # loss, img = visualize_filter(filter_index)
-    #
-    # return loss, img
--- API/main.py
+++ API/main.py
@@ -6,12 +6,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
-from drive.MyDrive.Exploring_The_Black_Box_Called_CNN.API import Visualizing_Filters as VF
+import seaborn as sns
+from Visualizing_CNN_Filters.API import Visualizing_Filters as VF
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 class Visualizer:
-  MAX_CONV_LAYER = 4
-  PATH = 'drive/some_model_using_convolutinal_layers'
-  PATH = 'drive/MyDrive/ITC_Final_Project/saved_models/CNN4_imgcropped'
   def __init__(self, model, conv_layer_name=None):
     self.__model = model
 
@@ -28,7 +28,7 @@ class Visualizer:
     else:
       conv_layers = [layer for layer in self.__model.layers if 'conv' in layer.name]
       number_of_conv_layers = len(conv_layers)
-      number_of_target_layer = min([self.MAX_CONV_LAYER, number_of_conv_layers-1])
+      number_of_target_layer = number_of_conv_layers
       layer = conv_layers[number_of_target_layer]
 
     # saves the target layer
@@ -37,11 +37,6 @@ class Visualizer:
     # creates the feature extractor
     self.__feature_extractor = keras.Model(inputs=model.inputs,
                                            outputs=layer.output)
-    
-    # save weights of target layer
-    w, b = layer.get_weights()
-    self.__target_layer_w = w
-    self.__target_layer_b = b
 
     # save the target layer location
     for i, layer in enumerate(model.layers):
@@ -53,12 +48,62 @@ class Visualizer:
   def set_target_layer(self, conv_layer_name):
       layer = self.__model.get_layer(name=conv_layer_name)
       self.__target_conv_layer = layer
-      self.__feature_extractor = keras.Model(inputs=self.__model.inputs,
+      self.__feature_extractor = keras.Model(inputs=model.inputs,
                                            outputs=layer.output)
+                                           
+    
+  def create_feature_extractor(self, layer_name):
+      layer = self.__model.get_layer(layer_name)
+      return keras.Model(inputs=self.__model.inputs, outputs=layer.output)
+    
+                                           
+  def get_activations(self, data, layer_name=None):
+      """returns the activations and the stadndart devaitations of them along the different images,
+         note: low stds may point on unneccesary filters"""
+      if layer_name:
+          feature_extractor = self.create_feature_extractor(layer_name)
+      else:
+          feature_extractor = self.__feature_extractor
+      acts = feature_extractor(data).numpy().mean(axis=(1,2))
+      stds = acts.std(axis=0)
+      return acts, stds, stds.argsort()
+      
+      
+  def evaluate_model(self, data, labels):
+      self.__model.evaluate(data, labels)
+      
+      
+  def evaluate_model_without_filter(self, data, labels, layer_name, filter_index, restore_weights=True):
+      def set_filter_weigths_to_zero(w,b,i):
+          # creates w and b with zeros for the i'th filter
+          # and leave the other filters intact
+          # maybe worth breaking the list comprehension for readability purpose
+          new_w = np.stack([np.zeros_like(w[:,:,:,i]) if j==i else w[:,:,:,j] for j in range(w.shape[-1])], -1)
+          new_b = np.stack([np.zeros_like(b[j]) if j==i else b[j] for j in range(w.shape[-1])], -1)
+          return new_w, new_b
+      
+      print('evaluation before:')
+      _, original_acc = self.__model.evaluate(data, labels)
+      
+      w,b = self.__model.get_layer(layer_name).weights
+      w, b = tf.identity(w), tf.identity(b)
+    #   new_w[...,filter_index] = tf.zeros_like(new_w[...,filter_index])
+    #   new_b[filter_index] = tf.zeros_like(new_b[filter_index])
+      self.__model.get_layer(layer_name).set_weights(set_filter_weigths_to_zero(w,b,filter_index))
+    #   self.__model.get_layer(layer_name).set_weights([new_w, new_b])
+      print('new evaluation:')
+      _, new_acc = self.__model.evaluate(data, labels)
+      
+      if type(restore_weights) in (int, float):
+        if new_acc+restore_weights < original_acc:
+          print(new_acc+restore_weights, original_acc)
+          self.__model.get_layer(layer_name).set_weights([w,b])
+      elif restore_weights==True:
+          self.__model.get_layer(layer_name).set_weights([w,b])
 
 
-  def print_activations(self, image, layer_name=None, layer_index=None, amount_of_filters=16):
-    # :input: image, either layer's index or layer's name
+  def print_activations(self, image, layer_name=None, amount_of_filters=16):
+    # :input: image, and layer's name
     # prints activations of all filters in a given layer
 
     if layer_name:
@@ -91,18 +136,42 @@ class Visualizer:
     pass
 
   
-  def cluster_activations(self, data=None, activations=None):
+  def cluster_activations(self, data=None, labels=None, activations=None):
     # either data or activations should be given to the method
-    # gives back best clustering of the inputs
-    # uses sklearn whatever clustering method
-    pass
+    # plots out the 2 PCA main components of the activations
+    if not activations:
+        activations = self.__feature_extractor()(data)
+        
+    scaler = StandardScaler()
+    scaled_acts = scaler.fit_transform(activations)
+    
+    pca = PCA(n_components=2)
+    res = pca.fit_transform(acts_scaled)
+    sns.scatterplot(x=res[:,0], y=res[:,1], hue=labels)
+    plt.title(f"PCA-ed activations")
+    plt.show()
 
   
-  def feature_visualization(self, filter_index=None, amount_of_filters=None, img_width=180, img_height=180):
+  def feature_visualization(self, filter_index,
+                            img_width=180, img_height=180,
+                            color_channels=3
+                            initializer=None, return_deprocessed=True,
+                            layer_name=None, iterations=30,
+                            return_deprocessed=True):
     # does feature visualiztion
     # uses another file
-    s = VF.visualize_filter(filter_index, self.__feature_extractor, img_width, img_height, self.__custom)
-    return s
+    if layer_name:
+        feature_extractor = create_feature_extractor()
+    else:
+        feature_extractor = self.__feature_extractor
+        
+    return VF.visualize_filter(filter_index, feature_extractor, 
+                                img_width, img_height, color_channels,
+                                self.__custom, 
+                                initializer=initializer,
+                                iterations=iterations,
+                                return_deprocessed=return_deprocessed)
+                            
 
   def redundancy_in_features(self, data, labels, var_1=None):
     # gets amount of redundant filters in the target conv layer
@@ -111,7 +180,7 @@ class Visualizer:
     original_evaluation = self.__model.evaluate(data, labels)
     self.__redundant = []
 
-    def set_i_filter_weigths_to_zero(self, w,b,i):
+    def set_i_filter_weigths_to_zero(w,b,i):
       # creates w and b with zeros for the i'th filter
       # and leave the other filters intact
       # maybe worth breaking the list comprehension for readability purpose
@@ -135,34 +204,5 @@ class Visualizer:
     return self.__feature_extractor
   def get_conv_layer(self):
     return self.__target_conv_layer
-  def get_3(self):
-    return self.__target_layer_w
-
-# model = keras.models.load_model(Visualizer.PATH)
-
-# # Build a ResNet50V2 model loaded with pre-trained ImageNet weights
-# model = keras.applications.ResNet50V2(weights="imagenet", include_top=False)
-# layer_name = "conv5_block3_3_conv"
-
-# # Set up a model that returns the activation values for our target layer
-# layer = model.get_layer(name=layer_name)
-# feature_extractor = keras.Model(inputs=model.inputs, outputs=layer.output)
-
-# vslr = Visualizer(model,
-#                   # layer_name
-#                   )
-#
-# loss, img = vslr.feature_visualization(filter_index=3, img_width=120, img_height=120)
-#
-# plt.imshow(img)
-# plt.show()
-#
-# import cv2
-#
-# img_resized = cv2.resize(img, (120,120))
-# img_resized.shape
-#
-# # [layer.name for layer in model.layers]
-# layer_name='conv2d_3'
-#
-# vslr.print_activations(img_resized, layer_name=layer_name)
+  def get_model(self):
+    return self.__model
